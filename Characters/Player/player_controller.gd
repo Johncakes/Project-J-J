85932672a3072ee1c614c602a06a54098f762455
julianarep <129572537+julianarep@users.noreply.github.com>
2025-05-08 13:07:11 +0900

    time mechanic

--- Characters/Player/player_controller.gd
+++ Characters/Player/player_controller.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
+const SPEED = 100
 
-func _physics_process(delta: float) -> void:
-    # Move the character based on input
-    var input_vector = Vector2.ZERO
-    if Input.is_action_pressed("ui_right"):
-        input_vector.x += 1
-    if Input.is_action_pressed("ui_left"):
-        input_vector.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        input_vector.y += 1
-    if Input.is_action_pressed("ui_up"):
-        input_vector.y -= 1
-    
-    input_vector = input_vector.normalized()
-    velocity = input_vector * speed
-    move_and_slide();
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	
+	velocity = direction.normalized() * SPEED
+	move_and_slide()
